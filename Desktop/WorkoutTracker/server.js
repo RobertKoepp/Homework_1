e6fb38af0aebde3937df22307b4560c7e2a693ef
Robert Koepp <rkoepp2@gmail.com>
2021-02-18 19:44:14 -0800

    changes

--- Desktop/WorkoutTracker/server.js
+++ Desktop/WorkoutTracker/server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/WorkoutTracker',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
