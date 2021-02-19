765fbd4816a90a535ad57b6ad364a4faceaf95d1
Robert Koepp <rkoepp2@gmail.com>
2021-02-18 19:30:36 -0800

    added mongo uri

--- Desktop/WorkoutTracker/server.js
+++ Desktop/WorkoutTracker/server.js
@@ -7,10 +7,15 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
